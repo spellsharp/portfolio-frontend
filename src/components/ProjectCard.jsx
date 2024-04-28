8f4bc94d797d26256f8119074f320914a1cc778c
spellsharp <svcodin22@gmail.com>
2024-04-28 21:37:54 +0530

    Removed tilt effect from back side of project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -28,30 +28,28 @@ const ProjectCardBack = ({
   link,
 }) => {
   return (
-    <Tilt>
-      <div className="h-80 rounded-md overflow-hidden border border-white border-opacity-10 shadow-black shadow-lg">
-        <div className="bg-[#292929b0] p-5 h-full flex flex-col justify-between">
-          <h1 className="text-white text-xl font-medium">{title}</h1>
-          <p className="text-slate-300">{description}</p>
+    <div className="h-80 rounded-md overflow-hidden border border-white border-opacity-10 shadow-black shadow-lg">
+      <div className="bg-[#292929b0] p-5 h-full flex flex-col justify-between">
+        <h1 className="text-white text-xl font-medium">{title}</h1>
+        <p className="text-slate-300">{description}</p>
 
-          <br />
-          <p className="text-white text-sm font-medium">{date}</p>
-          <p className="text-white text-sm font-medium">
-            Tech Stack: {techStack}
-          </p>
-          <div className="flex justify-between mt-5">
-            <a
-              href={github}
-              target="_blank"
-              rel="noreferrer"
-              className="text-white font-medium text-center hover:bg-blue-500 hover:bg-opacity-10 hover:shadow-[0_0_15px_1px_rgba(59,130,246,0.30)] rounded-full p-1 transition-all duration-300 ease-in-out"
-            >
-              GitHub
-            </a>
-          </div>
+        <br />
+        <p className="text-white text-sm font-medium">{date}</p>
+        <p className="text-white text-sm font-medium">
+          Tech Stack: {techStack}
+        </p>
+        <div className="flex justify-between mt-5">
+          <a
+            href={github}
+            target="_blank"
+            rel="noreferrer"
+            className="text-white font-medium text-center hover:bg-blue-500 hover:bg-opacity-10 hover:shadow-[0_0_15px_1px_rgba(59,130,246,0.30)] rounded-full p-1 transition-all duration-300 ease-in-out"
+          >
+            GitHub
+          </a>
         </div>
       </div>
-    </Tilt>
+    </div>
   );
 };
 
