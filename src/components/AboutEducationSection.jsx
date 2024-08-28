f8e37c59284674c03e1ff40e2034f05bd1509721
spellsharp <svcodin22@gmail.com>
2024-08-28 20:52:12 +1000

    Removed all parallax css from education and skills in about page

--- src/components/AboutEducationSection.jsx
+++ src/components/AboutEducationSection.jsx
@@ -5,7 +5,7 @@ import timelineData from '../content/timeline.json';
 
 const Parallax = () => {
   return (
-    <section className="flex flex-col items-center lg:justify-center md:justify-center sm:justify-start justify-start parallax parallax2">
+    <section className="flex flex-col items-center lg:justify-center md:justify-center sm:justify-start justify-start parallax parallax2 pb-10">
         <TimeLine data={timelineData} />
     </section>
   );
