80a8f3243e56a3ba816940e2f088b0aa27729660
spellsharp <svcodin22@gmail.com>
2024-08-28 22:27:48 +1000

    I forgot

--- src/components/contact/SocialMediaHandles.jsx
+++ src/components/contact/SocialMediaHandles.jsx
@@ -3,44 +3,36 @@ import { FaEnvelope, FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaShareAlt, F
 
 const SocialMediaHandles = () => {
   return (
-    <div className='flex flex-col space-y-10'>
+    <div className='flex flex-col space-y-4'>
         <div className="flex space-x-3 items-center">
-            <a href="mailto:shrisharanyan.vasu@gmail.com" target="_blank" rel="noreferrer" className="text-4xl text-blue-500">
+            <a href="mailto:shrisharanyan.vasu@gmail.com" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl text-blue-500">
                 <FaEnvelope />
             </a>
-            <div className='text-lg font-semibold'>Email: <span className='text-gray-400 font-normal'>shrisharanyan.vasu@gmail.com</span></div>
+            <div className='lg:text-lg md:text-lg sm:text-sm text-sm font-semibold'><span className='text-gray-400 font-normal'>shrisharanyan.vasu@gmail.com</span></div>
         </div>
         <div className='flex space-x-3 items-center'>
-            <div className="flex items-center space-x-3 text-4xl text-blue-500">
+            <div className="flex items-center space-x-3 lg:text-4xl md:text-4xl sm:text-xl text-xl text-blue-500">
                 <FaShareAlt />
-                <div className='text-lg font-semibold text-white'>Social Handles: </div>
+                <div className='lg:text-lg md:text-lg sm:text-sm text-sm font-semibold text-white'>Socials: </div>
             </div>
             <div className="flex justify-center space-x-4 text-gray-400">
-                <a href="https://twitter.com/shrisharanyan" target="_blank" rel="noreferrer" className="text-4xl hover:text-blue-500 transition-all duration-500">
+                <a href="https://twitter.com/shrisharanyan" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl hover:text-blue-500 transition-all duration-500">
                     <FaTwitter />
                 </a>
-                <a href="https://instagram.com/ssv_myself" target="_blank" rel="noreferrer" className="text-4xl hover:text-blue-500 transition-all duration-500">
+                <a href="https://instagram.com/ssv_myself" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl hover:text-blue-500 transition-all duration-500">
                     <FaInstagram />
                 </a>
-                <a href="https://linkedin.com/in/shrisharanyan" target="_blank" rel="noreferrer" className="text-4xl hover:text-blue-500 transition-all duration-500">
+                <a href="https://linkedin.com/in/shrisharanyan" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl hover:text-blue-500 transition-all duration-500">
                     <FaLinkedin />
                 </a>
-                <a href="https://kaggle.com/spellsharp" target="_blank" rel="noreferrer" className="text-4xl hover:text-blue-500 transition-all duration-500">
+                <a href="https://kaggle.com/spellsharp" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl hover:text-blue-500 transition-all duration-500">
                     <FaKaggle />
                 </a>
-                <a href="https://github.com/spellsharp" target="_blank" rel="noreferrer" className="text-4xl hover:text-blue-500 transition-all duration-500">
+                <a href="https://github.com/spellsharp" target="_blank" rel="noreferrer" className="lg:text-4xl md:text-4xl sm:text-xl text-xl hover:text-blue-500 transition-all duration-500">
                     <FaGithub />
                 </a>
             </div>
         </div>
-        <a
-          href="https://www.overleaf.com/read/tzrpndxvfxzk#b7e43f"
-          target="_blank"
-          rel='noreferrer'
-          className="sm:mx-auto mb-5 lg:mx-0 md:mx-0 mx-auto lg:p-3 lg:text-xl md:p-2 md:text-2xl sm:p-2 sm:text-xl max-w-fit p-2 text-xl shadow-[0_0_15px_1px_rgba(59,130,246,0.60)] border bg-blue-500 rounded-md border-white bg-opacity-0 lg:border-opacity-100 hover:bg-blue-500 hover:border-opacity-0 hover:transition-all duration-500 ease-in-out"
-        >
-          Curriculum Vitae
-        </a>
     </div>
   )
 }
--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Collaborate from "../../assets/contact/collaboration.svg";
+import Footer from "../../components/misc/Footer";
 
 import SocialMediaHandles from "../../components/contact/SocialMediaHandles";
 
@@ -7,12 +8,13 @@ const ContactPage = () => {
   return (
     <>
     <div className="min-h-screen flex lg:flex-row lg:space-x-5 md:flex-col md:space-y-3 sm:flex-col sm:space-y-3 flex-col space-y-3 justify-center items-center">
-      <div className="mt-20 flex flex-col text-center text-5xl items-center max-w-3xl">
+      <div className="mt-20 flex flex-col text-center lg:text-5xl md:text-5xl sm:text-3xl text-3xl items-center max-w-3xl">
         <div className="font-semibold">Let's work on something together!</div>
-        <img src={Collaborate} alt="Collaborate" className="w-1/2" />
+        <img src={Collaborate} alt="Collaborate" className="lg:w-1/2 md:w-1/2 sm:w-full" />
       </div>
       <SocialMediaHandles />
     </div>
+    <Footer />
   </>
   );
 };
